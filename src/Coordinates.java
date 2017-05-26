ca832227a1bcc6b0abc8fe499ffaacb03d384c7e
B46qqq <B46qqq@users.noreply.github.com>
2017-05-26 19:58:55 +1000

    Update Coordinates.java

--- src/Coordinates.java
+++ src/Coordinates.java
@@ -1,7 +1,14 @@
 /**
- * Stores coordiantes of a Sprite
- * @author martinle
+ * c-2911 Group Project
+ * Members :: {
+ * @author     Alan Wan     z5076302
+ * @author     Allan Lai    z5117352
+ * @author     Martin Le    z3466361
+ * @author     Zhaohan Bao  z5114676
+ *            }
+ * @version 5.0
  *
+ * Class for spirtes 
  */
 public class Coordinates {
 	//Sprite codes:
