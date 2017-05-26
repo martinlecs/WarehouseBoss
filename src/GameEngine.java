c1eeb27f5302f18cd5e72ffd04222de692323fc2
B46qqq <B46qqq@users.noreply.github.com>
2017-05-26 20:07:27 +1000

    javadoc update

--- src/GameEngine.java
+++ src/GameEngine.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
  * @author     Alan Wan     z5076302
  * @author     Allan Lai    z5117352
  * @author     Martin Le    z3466361
- * @author     Zhaohan Bao  z5114676 (Tony)
+ * @author     Zhaohan Bao  z5114676
  *            }
  * @version 5.0
  *
@@ -75,57 +75,49 @@ public class GameEngine implements Constants, KeyListener{
         int nextX = playerPosition.get(X) + dx;
         int nextY = playerPosition.get(Y) + dy;
 
-        if (nextPixelType == null) return;
-        // if the next move is NOT valid. example, wall block
-        if (nextPixelType == INVALID || nextPixelType == WALL) return;
-
-        // if current position is a combination pixel block
-        // combination as in player on top of a goal pixel or a box reached a goal state
-        // if the next move is not valid, for example wall blocked, then simple return and do nothing.
+        if (nextPixelType == null) return;                  // if the next move is NOT valid. example, wall block
         if (nextPixelType == INVALID || nextPixelType == WALL) return;
 
         int nextNextX = playerPosition.get(X) + (2 * dx);
         int nextNextY = playerPosition.get(Y) + (2 * dy);
-        // if next move is a road. Update the next and curr pixel block
-        if (nextPixelType == ROAD){
+
+        if (nextPixelType == ROAD){                         // if next move is a road. Update the next and curr pixel block
             if (map.isPlayerOnGoal())
-                map.setXY(playerPosition.get(X), playerPosition.get(Y), GOAL); // update current pixel
+                map.setXY(playerPosition.get(X), playerPosition.get(Y), GOAL); 
             else
-                map.setXY(playerPosition.get(X), playerPosition.get(Y), ROAD); // update current pixel
-            map.setXY(nextX, nextY, PLAYER);                                // update next pixel
-            map.setPlayerPosition(nextX, nextY);                            // update player position
+                map.setXY(playerPosition.get(X), playerPosition.get(Y), ROAD); 
+            map.setXY(nextX, nextY, PLAYER);
+            map.setPlayerPosition(nextX, nextY);
         }else if (nextPixelType == BOX || nextPixelType == GOAL_REACHED){    // if next pixel is a box
-            // since we will be pushing a box, check if the box is movable
-            if (nextNextPixelType == null) return;
+            if (nextNextPixelType == null) return;                          // since we will be pushing a box, check if the box is movable
             if (nextNextPixelType == INVALID || nextNextPixelType == WALL ||
                     nextNextPixelType == BOX || nextNextPixelType == GOAL_REACHED) return;
-            // once we checked that the box is movable
-            // we can be sure that the current pixel and the next pixel is going to be.
-            if (map.isPlayerOnGoal())
-                map.setXY(playerPosition.get(X), playerPosition.get(Y), GOAL); // update current pixel
+
+            if (map.isPlayerOnGoal())                                           // once we checked that the box is movable
+                map.setXY(playerPosition.get(X), playerPosition.get(Y), GOAL);  // we can be sure that the current pixel and the next pixel is going to be.
             else
-                map.setXY(playerPosition.get(X), playerPosition.get(Y), ROAD); // update current pixel
-            map.setPlayerPosition(nextX, nextY);                           // update player position
+                map.setXY(playerPosition.get(X), playerPosition.get(Y), ROAD); 
+            map.setPlayerPosition(nextX, nextY);
 
             if (nextPixelType == BOX)
-                map.setXY(nextX, nextY, PLAYER);                               // update next pixel to player
+                map.setXY(nextX, nextY, PLAYER);                      
             else if (nextPixelType == GOAL_REACHED){
-                map.setXY(nextX, nextY, PLAYER_ON_GOAL);                               // update next pixel to player
+                map.setXY(nextX, nextY, PLAYER_ON_GOAL);
                 map.undoGoal();
             }
-            if (nextNextPixelType == ROAD)  // if next next pixel is road/empty
-                map.setXY(nextNextX, nextNextY, BOX);                       // update current pixel
-            else if (nextNextPixelType == GOAL) { // when next next pixel if the goal
+            if (nextNextPixelType == ROAD)                                  // if next next pixel is road/empty
+                map.setXY(nextNextX, nextNextY, BOX);                       
+            else if (nextNextPixelType == GOAL) {                           // when next next pixel if the goal
                 map.setXY(nextNextX, nextNextY, GOAL_REACHED);              // GOAL REACHED !!
                 map.doneGoal();
             }
-        }else if (nextPixelType == GOAL) {   // if the next pixel is a goal --> lead to new image
+        }else if (nextPixelType == GOAL) {                                  // if the next pixel is a goal --> lead to new image
             if (map.isPlayerOnGoal())
-                map.setXY(playerPosition.get(X), playerPosition.get(Y), GOAL); // update current pixel
+                map.setXY(playerPosition.get(X), playerPosition.get(Y), GOAL);
             else
-                map.setXY(playerPosition.get(X), playerPosition.get(Y), ROAD); // update current pixel
+                map.setXY(playerPosition.get(X), playerPosition.get(Y), ROAD);
             map.setXY(nextX, nextY, PLAYER_ON_GOAL);
-            map.setPlayerPosition(nextX, nextY);                            // update player position
+            map.setPlayerPosition(nextX, nextY);
         }
     }
 
@@ -168,13 +160,13 @@ public class GameEngine implements Constants, KeyListener{
     }
 
     /**
-     *
-     * @param e
+     * Built-in interface class listen to what key the user has typed.
+     * Once user typed registered keys on the keyboard, 
+     * if migh lead to changes in the game based on its validation.
+     * @param e data regarding the key pressed
      */
     @Override
     public void keyPressed(KeyEvent e) {
-        //if (map.getGoal() == 0) gameEnd();
-
         Integer move    = null;
         Integer dx      = null;
         Integer dy      = null;
@@ -212,11 +204,9 @@ public class GameEngine implements Constants, KeyListener{
         }
         moveValidation2 (dx, dy);
         map.updatePlayerDirection(move);
-        //map.displayMap(); // for debugging purposes
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-
     }
 }
