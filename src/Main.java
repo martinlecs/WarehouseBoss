0ae4dded6b44a154b8703bae601b3a131a9f75cc
Martin Le <martinle.cse@gmail.com>
2017-05-26 16:11:11 +1000

    included unix executables

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
  * Created by b46qqq on 10/5/17.
  */
 
-public class Game implements Constants{
+public class Main implements Constants{
 
     public static void main (String[] args){
         new GameStartMenu();
