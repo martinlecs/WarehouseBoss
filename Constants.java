665f394fa7a60cabd1422b3d9c59ac542bf999af
B46qqq <B46qqq@users.noreply.github.com>
2017-05-26 12:23:31 +1000

    final ver

---
+++ Constants.java
@@ -0,0 +1,33 @@
+/**
+ * Created by b46qqq on 1/5/17.
+ */
+public interface Constants {
+    public final Integer INVALID = -1;
+    public final Integer PLAYER  = 0;
+    public final Integer BOX     = 1;
+    public final Integer ROAD    = 2;
+    public final Integer GOAL    = 3;
+    public final Integer WALL    = 4;
+    public final Integer GOAL_REACHED = 5;
+    public final Integer PLAYER_ON_GOAL = 6;
+    public final Integer PLAYER_FACE_UP  = 7;
+    public final Integer PLAYER_FACE_DOWN  = 8;
+    public final Integer PLAYER_FACE_LEFT  = 9;
+    public final Integer PLAYER_FACE_RIGHT  = 10;
+    public final Integer PLAYER_FACE_UP_ON_GOAL  = 11;
+    public final Integer PLAYER_FACE_DOWN_ON_GOAL  = 12;
+    public final Integer PLAYER_FACE_LEFT_ON_GOAL  = 13;
+    public final Integer PLAYER_FACE_RIGHT_ON_GOAL  = 14;
+
+    public final Integer X = 0;
+    public final Integer Y = 1;
+    public final Integer TYPE = 2;
+
+    public final Integer UP      = 0;
+    public final Integer DOWN    = 1;
+    public final Integer LEFT    = 2;
+    public final Integer RIGHT   = 3;
+
+    public final Integer RESET   = 101;
+    public final Integer EXIT    = 102;
+}
